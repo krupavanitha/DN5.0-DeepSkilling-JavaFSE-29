a989a5255d824993bd555437ba7c38943d52948c
krupavanitha <mukkukrupavanitha@gmail.com>
2026-06-21 14:52:24 +0530

    Add Main class to demonstrate Singleton pattern

---
+++ Design Patterns and Principles/SingletonPatternExample/Design Patterns and Principles/SingletonPatternExample/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        Logger logger1 = Logger.getInstance();
+        Logger logger2 = Logger.getInstance();
+
+        logger1.log("Application Started");
+        logger2.log("Application Running");
+
+        if (logger1 == logger2) {
+            System.out.println("Only One Logger Instance Exists");
+        }
+    }
+}
