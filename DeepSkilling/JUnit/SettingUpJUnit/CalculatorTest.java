b10ed85495a292374be67cdf5ada3b0a8d5e9382
krupavanitha <mukkukrupavanitha@gmail.com>
2026-07-09 14:55:11 +0530

    Add JUnit test for addition in Calculator

---
+++ DeepSkilling/JUnit/SettingUpJUnit/CalculatorTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.assertEquals;
+import org.junit.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void testAddition() {
+
+        int result = 10 + 20;
+
+        assertEquals(30, result);
+    }
+}
