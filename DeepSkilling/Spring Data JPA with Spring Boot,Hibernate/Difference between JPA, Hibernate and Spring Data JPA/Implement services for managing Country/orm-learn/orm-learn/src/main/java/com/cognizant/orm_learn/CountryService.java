a0aa5350d2f43e30b570c99210307debc6435600
krupavanitha <mukkukrupavanitha@gmail.com>
2026-07-25 20:31:04 +0530

    Implement services for managing Country

---
+++ DeepSkilling/Spring Data JPA with Spring Boot,Hibernate/Difference between JPA, Hibernate and Spring Data JPA/Implement services for managing Country/orm-learn/orm-learn/src/main/java/com/cognizant/orm_learn/CountryService.java
@@ -0,0 +1,19 @@
+package com.cognizant.orm_learn;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Transactional
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+}
