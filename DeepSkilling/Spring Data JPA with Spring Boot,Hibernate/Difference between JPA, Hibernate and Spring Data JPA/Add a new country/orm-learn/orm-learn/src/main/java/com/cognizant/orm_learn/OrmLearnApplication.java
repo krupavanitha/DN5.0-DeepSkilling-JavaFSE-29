fe0479d7af28c8967ecf29d5c90e29c2613bd8e4
krupavanitha <mukkukrupavanitha@gmail.com>
2026-07-25 20:58:25 +0530

    Add a new country

---
+++ DeepSkilling/Spring Data JPA with Spring Boot,Hibernate/Difference between JPA, Hibernate and Spring Data JPA/Add a new country/orm-learn/orm-learn/src/main/java/com/cognizant/orm_learn/OrmLearnApplication.java
@@ -0,0 +1,27 @@
+package com.cognizant.orm_learn;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmLearnApplication implements CommandLineRunner {
+
+    @Autowired
+    private CountryService countryService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrmLearnApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Country country = new Country("ZZ", "Zootopia");
+
+        countryService.addCountry(country);
+
+        System.out.println(countryService.getCountry("ZZ"));
+    }
+}
