219c23e27d18b8a927f30b3d49ca21550f66c835
krupavanitha <mukkukrupavanitha@gmail.com>
2026-07-25 21:02:11 +0530

    Update an existing country

---
+++ DeepSkilling/Spring Data JPA with Spring Boot,Hibernate/Difference between JPA, Hibernate and Spring Data JPA/Update an existing country/orm-learn/orm-learn/src/main/java/com/cognizant/orm_learn/OrmLearnApplication.java
@@ -0,0 +1,29 @@
+package com.cognizant.orm_learn;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmLearnApplication implements CommandLineRunner {
+
+    @Autowired
+    private CountryService countryService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrmLearnApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        Country country = countryService.getCountry("ZZ");
+
+        country.setName("Zootopia Updated");
+
+        countryService.addCountry(country);
+
+        System.out.println(countryService.getCountry("ZZ"));
+    }
+}
