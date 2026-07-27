6572a815f20697c8b6adaadb2adbed3bcf2e1915
krupavanitha <mukkukrupavanitha@gmail.com>
2026-07-27 20:31:43 +0530

    Get countries starting with alphabet

---
+++ DeepSkilling/Spring Data JPA with Spring Boot,Hibernate/Difference between JPA, Hibernate and Spring Data JPA/Get countries starting with alphabet/orm-learn/orm-learn/src/main/java/com/cognizant/orm_learn/CountryService.java
@@ -0,0 +1,36 @@
+package com.cognizant.orm_learn;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+public class CountryService {
+
+    @Autowired
+    private CountryRepository countryRepository;
+
+    @Transactional
+    public List<Country> getAllCountries() {
+        return countryRepository.findAll();
+    }
+
+    @Transactional
+    public Country getCountry(String code) {
+        return countryRepository.findByCode(code);
+    }
+   @Transactional
+public void addCountry(Country country) {
+    countryRepository.save(country);
+}
+@Transactional
+public void deleteCountry(String code) {
+    countryRepository.deleteByCode(code);
+}
+@Transactional
+public List<Country> getCountriesStartingWith(String alphabet) {
+    return countryRepository.findByNameStartingWith(alphabet);
+}
+}
---
+++ DeepSkilling/Spring Data JPA with Spring Boot,Hibernate/Difference between JPA, Hibernate and Spring Data JPA/Get countries starting with alphabet/orm-learn/orm-learn/src/main/java/com/cognizant/orm_learn/OrmLearnApplication.java
@@ -0,0 +1,25 @@
+package com.cognizant.orm_learn;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmLearnApplication implements CommandLineRunner {
+
+    @Autowired
+    private CountryService countryService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrmLearnApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        countryService.getCountriesStartingWith("I")
+                .forEach(System.out::println);
+
+    }
+}
