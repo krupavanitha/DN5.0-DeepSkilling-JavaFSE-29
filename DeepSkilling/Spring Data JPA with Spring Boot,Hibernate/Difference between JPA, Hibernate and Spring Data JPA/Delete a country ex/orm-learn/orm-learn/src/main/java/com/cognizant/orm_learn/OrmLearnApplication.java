76f4c0eb808effc8c006a5cf4176337b33bbecef
krupavanitha <mukkukrupavanitha@gmail.com>
2026-07-27 20:14:06 +0530

    Delete a country

---
+++ DeepSkilling/Spring Data JPA with Spring Boot,Hibernate/Difference between JPA, Hibernate and Spring Data JPA/Delete a country ex/orm-learn/orm-learn/src/main/java/com/cognizant/orm_learn/OrmLearnApplication.java
@@ -0,0 +1,27 @@
+package com.cognizant.orm_learn;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrmLearnApplication implements CommandLineRunner {
+
+    @Autowired
+    private CountryService countryService;
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrmLearnApplication.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+
+        countryService.deleteCountry("ZZ");
+
+        Country country = countryService.getCountry("ZZ");
+
+        System.out.println(country);
+    }
+}
