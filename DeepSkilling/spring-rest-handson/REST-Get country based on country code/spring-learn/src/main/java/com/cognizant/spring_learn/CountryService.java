f3d955847d6f7dd65cb4688f1d88d2322bbb4730
krupavanitha <mukkukrupavanitha@gmail.com>
2026-07-28 15:08:51 +0530

    Completed Spring REST Hands-on - Get Country Based on Country Code

---
+++ DeepSkilling/spring-rest-handson/REST-Get country based on country code/spring-learn/src/main/java/com/cognizant/spring_learn/CountryService.java
@@ -0,0 +1,30 @@
+package com.cognizant.spring_learn;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class CountryService {
+
+    private static List<Country> countryList = new ArrayList<>();
+
+    static {
+        countryList.add(new Country("IN", "India"));
+        countryList.add(new Country("US", "United States"));
+        countryList.add(new Country("JP", "Japan"));
+    }
+
+    public List<Country> getAllCountries() {
+        return countryList;
+    }
+public Country getCountry(String code) {
+    for (Country country : countryList) {
+        if (country.getCode().equalsIgnoreCase(code)) {
+            return country;
+        }
+    }
+    return null;
+}
+}
