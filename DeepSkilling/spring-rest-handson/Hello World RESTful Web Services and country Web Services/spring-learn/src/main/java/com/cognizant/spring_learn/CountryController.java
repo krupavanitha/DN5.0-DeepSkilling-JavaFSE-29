739fbe80ca2b8148604a9d733583b55287841ce2
krupavanitha <mukkukrupavanitha@gmail.com>
2026-07-28 15:02:44 +0530

    Completed Spring REST Hands-on - Hello World RESTful Web Service and Country Web Service

---
+++ DeepSkilling/spring-rest-handson/Hello World RESTful Web Services and country Web Services/spring-learn/src/main/java/com/cognizant/spring_learn/CountryController.java
@@ -0,0 +1,19 @@
+package com.cognizant.spring_learn;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class CountryController {
+
+    @Autowired
+    private CountryService countryService;
+
+    @GetMapping("/country")
+    public List<Country> getAllCountries() {
+        return countryService.getAllCountries();
+    }
+}
