ee4c54d316ff891ab4342dc5198227fedf9b07a9
krupavanitha <mukkukrupavanitha@gmail.com>
2026-07-29 22:26:10 +0530

    ReactJS-HOL 5 – Styling React Components

---
+++ DeepSkilling/React/ReactJS-HOL 5 – Styling React Components/src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="container">
+      <h1>Student Profile</h1>
+
+      <div className="card">
+        <h2>Krupa</h2>
+        <p>B.Tech - CSE</p>
+        <p>Vignan Institute of Engineering for Women</p>
+      </div>
+
+      <div className="card">
+        <h2>React Styling HOL</h2>
+        <p>Learning CSS in React</p>
+      </div>
+    </div>
+  );
+}
+
+export default App;
